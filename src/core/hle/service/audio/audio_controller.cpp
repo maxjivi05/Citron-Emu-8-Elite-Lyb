238e8282b2a0f1eb9f44f4a26bb88ecc7db286cb
Zephyron <zephyron@citron-emu.org>
2025-05-24 16:39:15 +1000

    hle/service/audio: Implement missing audio services and functions
    - Add complete IAudioController implementation with 32 missing functions:
      * Volume control (GetTargetVolume, SetTargetVolume, etc.)
      * Mute control (IsTargetMute, SetTargetMute, etc.)
      * Target management (SetDefaultTarget, GetDefaultTarget, etc.)
      * Play report functions (10000+ series command IDs)
      * Hearing protection safeguard functions
      * Debug and system functions
    
    - Add new audio services:
      * codecctl - Codec controller service with 13 functions
      * auddebug - Audio debug service placeholder
      * Device suspend/resume services (audout:d, audin:d, audrec:d, audren:d)
      * System audio services (aud:a, aud:d)
      * Applet audio services (audout:a, audin:a, audren:a)
    
    - Update IAudioDevice with missing functions:
      * GetAudioSystemMasterVolumeSetting
      * GetActiveAudioOutputDeviceName
      * Fix function ID mappings to match official documentation
    
    - Register all services in main audio LoopProcess function
    - Update CMakeLists.txt with new source files
    
    All implementations follow the official Switch audio services documentation
    from switchbrew.org and provide appropriate stub implementations with
    sensible default values for enhanced game compatibility.
    
    Signed-off-by: Zephyron <zephyron@citron-emu.org>

--- src/core/hle/service/audio/audio_controller.cpp
+++ src/core/hle/service/audio/audio_controller.cpp
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2018 yuzu Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025 citron Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "common/logging/log.h"
@@ -14,60 +15,60 @@ IAudioController::IAudioController(Core::System& system_)
     : ServiceFramework{system_, "audctl"}, service_context{system, "audctl"} {
     // clang-format off
     static const FunctionInfo functions[] = {
-        {0, nullptr, "GetTargetVolume"},
-        {1, nullptr, "SetTargetVolume"},
+        {0, D<&IAudioController::GetTargetVolume>, "GetTargetVolume"},
+        {1, D<&IAudioController::SetTargetVolume>, "SetTargetVolume"},
         {2, D<&IAudioController::GetTargetVolumeMin>, "GetTargetVolumeMin"},
         {3, D<&IAudioController::GetTargetVolumeMax>, "GetTargetVolumeMax"},
-        {4, nullptr, "IsTargetMute"},
-        {5, nullptr, "SetTargetMute"},
-        {6, nullptr, "IsTargetConnected"},
-        {7, nullptr, "SetDefaultTarget"},
-        {8, nullptr, "GetDefaultTarget"},
+        {4, D<&IAudioController::IsTargetMute>, "IsTargetMute"},
+        {5, D<&IAudioController::SetTargetMute>, "SetTargetMute"},
+        {6, D<&IAudioController::IsTargetConnected>, "IsTargetConnected"},
+        {7, D<&IAudioController::SetDefaultTarget>, "SetDefaultTarget"},
+        {8, D<&IAudioController::GetDefaultTarget>, "GetDefaultTarget"},
         {9, D<&IAudioController::GetAudioOutputMode>, "GetAudioOutputMode"},
         {10, D<&IAudioController::SetAudioOutputMode>, "SetAudioOutputMode"},
-        {11, nullptr, "SetForceMutePolicy"},
+        {11, D<&IAudioController::SetForceMutePolicy>, "SetForceMutePolicy"},
         {12, D<&IAudioController::GetForceMutePolicy>, "GetForceMutePolicy"},
         {13, D<&IAudioController::GetOutputModeSetting>, "GetOutputModeSetting"},
         {14, D<&IAudioController::SetOutputModeSetting>, "SetOutputModeSetting"},
-        {15, nullptr, "SetOutputTarget"},
-        {16, nullptr, "SetInputTargetForceEnabled"},
+        {15, D<&IAudioController::SetOutputTarget>, "SetOutputTarget"},
+        {16, D<&IAudioController::SetInputTargetForceEnabled>, "SetInputTargetForceEnabled"},
         {17, D<&IAudioController::SetHeadphoneOutputLevelMode>, "SetHeadphoneOutputLevelMode"},
         {18, D<&IAudioController::GetHeadphoneOutputLevelMode>, "GetHeadphoneOutputLevelMode"},
-        {19, nullptr, "AcquireAudioVolumeUpdateEventForPlayReport"},
-        {20, nullptr, "AcquireAudioOutputDeviceUpdateEventForPlayReport"},
-        {21, nullptr, "GetAudioOutputTargetForPlayReport"},
+        {19, D<&IAudioController::AcquireAudioVolumeUpdateEventForPlayReport>, "AcquireAudioVolumeUpdateEventForPlayReport"},
+        {20, D<&IAudioController::AcquireAudioOutputDeviceUpdateEventForPlayReport>, "AcquireAudioOutputDeviceUpdateEventForPlayReport"},
+        {21, D<&IAudioController::GetAudioOutputTargetForPlayReport>, "GetAudioOutputTargetForPlayReport"},
         {22, D<&IAudioController::NotifyHeadphoneVolumeWarningDisplayedEvent>, "NotifyHeadphoneVolumeWarningDisplayedEvent"},
-        {23, nullptr, "SetSystemOutputMasterVolume"},
-        {24, nullptr, "GetSystemOutputMasterVolume"},
-        {25, nullptr, "GetAudioVolumeDataForPlayReport"},
-        {26, nullptr, "UpdateHeadphoneSettings"},
-        {27, nullptr, "SetVolumeMappingTableForDev"},
-        {28, nullptr, "GetAudioOutputChannelCountForPlayReport"},
-        {29, nullptr, "BindAudioOutputChannelCountUpdateEventForPlayReport"},
+        {23, D<&IAudioController::SetSystemOutputMasterVolume>, "SetSystemOutputMasterVolume"},
+        {24, D<&IAudioController::GetSystemOutputMasterVolume>, "GetSystemOutputMasterVolume"},
+        {25, D<&IAudioController::GetAudioVolumeDataForPlayReport>, "GetAudioVolumeDataForPlayReport"},
+        {26, D<&IAudioController::UpdateHeadphoneSettings>, "UpdateHeadphoneSettings"},
+        {27, D<&IAudioController::SetVolumeMappingTableForDev>, "SetVolumeMappingTableForDev"},
+        {28, D<&IAudioController::GetAudioOutputChannelCountForPlayReport>, "GetAudioOutputChannelCountForPlayReport"},
+        {29, D<&IAudioController::BindAudioOutputChannelCountUpdateEventForPlayReport>, "BindAudioOutputChannelCountUpdateEventForPlayReport"},
         {30, D<&IAudioController::SetSpeakerAutoMuteEnabled>, "SetSpeakerAutoMuteEnabled"},
         {31, D<&IAudioController::IsSpeakerAutoMuteEnabled>, "IsSpeakerAutoMuteEnabled"},
-        {32, nullptr, "GetActiveOutputTarget"},
-        {33, nullptr, "GetTargetDeviceInfo"},
+        {32, D<&IAudioController::GetActiveOutputTarget>, "GetActiveOutputTarget"},
+        {33, D<&IAudioController::GetTargetDeviceInfo>, "GetTargetDeviceInfo"},
         {34, D<&IAudioController::AcquireTargetNotification>, "AcquireTargetNotification"},
-        {35, nullptr, "SetHearingProtectionSafeguardTimerRemainingTimeForDebug"},
-        {36, nullptr, "GetHearingProtectionSafeguardTimerRemainingTimeForDebug"},
-        {37, nullptr, "SetHearingProtectionSafeguardEnabled"},
-        {38, nullptr, "IsHearingProtectionSafeguardEnabled"},
-        {39, nullptr, "IsHearingProtectionSafeguardMonitoringOutputForDebug"},
-        {40, nullptr, "GetSystemInformationForDebug"},
-        {41, nullptr, "SetVolumeButtonLongPressTime"},
-        {42, nullptr, "SetNativeVolumeForDebug"},
-        {10000, nullptr, "NotifyAudioOutputTargetForPlayReport"},
-        {10001, nullptr, "NotifyAudioOutputChannelCountForPlayReport"},
-        {10002, nullptr, "NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport"},
-        {10100, nullptr, "GetAudioVolumeDataForPlayReport"},
-        {10101, nullptr, "BindAudioVolumeUpdateEventForPlayReport"},
-        {10102, nullptr, "BindAudioOutputTargetUpdateEventForPlayReport"},
-        {10103, nullptr, "GetAudioOutputTargetForPlayReport"},
-        {10104, nullptr, "GetAudioOutputChannelCountForPlayReport"},
-        {10105, nullptr, "BindAudioOutputChannelCountUpdateEventForPlayReport"},
-        {10106, nullptr, "GetDefaultAudioOutputTargetForPlayReport"},
-        {50000, nullptr, "SetAnalogInputBoostGainForPrototyping"},
+        {35, D<&IAudioController::SetHearingProtectionSafeguardTimerRemainingTimeForDebug>, "SetHearingProtectionSafeguardTimerRemainingTimeForDebug"},
+        {36, D<&IAudioController::GetHearingProtectionSafeguardTimerRemainingTimeForDebug>, "GetHearingProtectionSafeguardTimerRemainingTimeForDebug"},
+        {37, D<&IAudioController::SetHearingProtectionSafeguardEnabled>, "SetHearingProtectionSafeguardEnabled"},
+        {38, D<&IAudioController::IsHearingProtectionSafeguardEnabled>, "IsHearingProtectionSafeguardEnabled"},
+        {39, D<&IAudioController::IsHearingProtectionSafeguardMonitoringOutputForDebug>, "IsHearingProtectionSafeguardMonitoringOutputForDebug"},
+        {40, D<&IAudioController::GetSystemInformationForDebug>, "GetSystemInformationForDebug"},
+        {41, D<&IAudioController::SetVolumeButtonLongPressTime>, "SetVolumeButtonLongPressTime"},
+        {42, D<&IAudioController::SetNativeVolumeForDebug>, "SetNativeVolumeForDebug"},
+        {10000, D<&IAudioController::NotifyAudioOutputTargetForPlayReport>, "NotifyAudioOutputTargetForPlayReport"},
+        {10001, D<&IAudioController::NotifyAudioOutputChannelCountForPlayReport>, "NotifyAudioOutputChannelCountForPlayReport"},
+        {10002, D<&IAudioController::NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport>, "NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport"},
+        {10100, D<&IAudioController::GetAudioVolumeDataForPlayReport>, "GetAudioVolumeDataForPlayReport"},
+        {10101, D<&IAudioController::BindAudioVolumeUpdateEventForPlayReport>, "BindAudioVolumeUpdateEventForPlayReport"},
+        {10102, D<&IAudioController::BindAudioOutputTargetUpdateEventForPlayReport>, "BindAudioOutputTargetUpdateEventForPlayReport"},
+        {10103, D<&IAudioController::GetAudioOutputTargetForPlayReport>, "GetAudioOutputTargetForPlayReport"},
+        {10104, D<&IAudioController::GetAudioOutputChannelCountForPlayReport>, "GetAudioOutputChannelCountForPlayReport"},
+        {10105, D<&IAudioController::BindAudioOutputChannelCountUpdateEventForPlayReport>, "BindAudioOutputChannelCountUpdateEventForPlayReport"},
+        {10106, D<&IAudioController::GetDefaultAudioOutputTargetForPlayReport>, "GetDefaultAudioOutputTargetForPlayReport"},
+        {50000, D<&IAudioController::SetAnalogInputBoostGainForPrototyping>, "SetAnalogInputBoostGainForPrototyping"},
     };
     // clang-format on
 
@@ -173,6 +174,210 @@ Result IAudioController::AcquireTargetNotification(
     LOG_WARNING(Service_AM, "(STUBBED) called");
 
     *out_notification_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::GetTargetVolume(Out<s32> out_target_volume) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_target_volume = 10;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetTargetVolume(s32 target_volume) {
+    LOG_WARNING(Audio, "(STUBBED) called, target_volume={}", target_volume);
+    R_SUCCEED();
+}
+
+Result IAudioController::IsTargetMute(Out<bool> out_is_target_mute) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_is_target_mute = false;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetTargetMute(bool is_target_mute) {
+    LOG_WARNING(Audio, "(STUBBED) called, is_target_mute={}", is_target_mute);
+    R_SUCCEED();
+}
+
+Result IAudioController::IsTargetConnected(Out<bool> out_is_target_connected) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_is_target_connected = true;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetDefaultTarget(u32 target) {
+    LOG_WARNING(Audio, "(STUBBED) called, target={}", target);
+    R_SUCCEED();
+}
+
+Result IAudioController::GetDefaultTarget(Out<u32> out_target) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_target = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetForceMutePolicy(ForceMutePolicy force_mute_policy) {
+    LOG_WARNING(Audio, "(STUBBED) called, force_mute_policy={}", static_cast<u32>(force_mute_policy));
+    R_SUCCEED();
+}
+
+Result IAudioController::SetOutputTarget(u32 target) {
+    LOG_WARNING(Audio, "(STUBBED) called, target={}", target);
+    R_SUCCEED();
+}
+
+Result IAudioController::SetInputTargetForceEnabled(bool is_force_enabled) {
+    LOG_WARNING(Audio, "(STUBBED) called, is_force_enabled={}", is_force_enabled);
+    R_SUCCEED();
+}
+
+Result IAudioController::AcquireAudioVolumeUpdateEventForPlayReport(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::AcquireAudioOutputDeviceUpdateEventForPlayReport(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::GetAudioOutputTargetForPlayReport(Out<u32> out_target) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_target = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetSystemOutputMasterVolume(f32 volume) {
+    LOG_INFO(Audio, "called, volume={}", volume);
+    R_SUCCEED();
+}
+
+Result IAudioController::GetSystemOutputMasterVolume(Out<f32> out_volume) {
+    LOG_INFO(Audio, "called");
+    *out_volume = 1.0f;
+    R_SUCCEED();
+}
+
+Result IAudioController::GetAudioVolumeDataForPlayReport(Out<u32> out_volume_data) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_volume_data = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::UpdateHeadphoneSettings() {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    R_SUCCEED();
+}
+
+Result IAudioController::SetVolumeMappingTableForDev(InLargeData<u32, BufferAttr_HipcMapAlias> volume_mapping_table) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    R_SUCCEED();
+}
+
+Result IAudioController::GetAudioOutputChannelCountForPlayReport(Out<u32> out_channel_count) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_channel_count = 2;
+    R_SUCCEED();
+}
+
+Result IAudioController::BindAudioOutputChannelCountUpdateEventForPlayReport(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::GetActiveOutputTarget(Out<u32> out_target) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_target = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::GetTargetDeviceInfo(Out<u32> out_device_info, u32 target) {
+    LOG_WARNING(Audio, "(STUBBED) called, target={}", target);
+    *out_device_info = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetHearingProtectionSafeguardTimerRemainingTimeForDebug(u64 remaining_time) {
+    LOG_WARNING(Audio, "(STUBBED) called, remaining_time={}", remaining_time);
+    R_SUCCEED();
+}
+
+Result IAudioController::GetHearingProtectionSafeguardTimerRemainingTimeForDebug(Out<u64> out_remaining_time) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_remaining_time = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetHearingProtectionSafeguardEnabled(bool is_enabled) {
+    LOG_WARNING(Audio, "(STUBBED) called, is_enabled={}", is_enabled);
+    R_SUCCEED();
+}
+
+Result IAudioController::IsHearingProtectionSafeguardEnabled(Out<bool> out_is_enabled) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_is_enabled = false;
+    R_SUCCEED();
+}
+
+Result IAudioController::IsHearingProtectionSafeguardMonitoringOutputForDebug(Out<bool> out_is_monitoring) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_is_monitoring = false;
+    R_SUCCEED();
+}
+
+Result IAudioController::GetSystemInformationForDebug(OutLargeData<u8, BufferAttr_HipcMapAlias> out_info) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    R_SUCCEED();
+}
+
+Result IAudioController::SetVolumeButtonLongPressTime(u64 long_press_time) {
+    LOG_WARNING(Audio, "(STUBBED) called, long_press_time={}", long_press_time);
+    R_SUCCEED();
+}
+
+Result IAudioController::SetNativeVolumeForDebug(f32 native_volume) {
+    LOG_WARNING(Audio, "(STUBBED) called, native_volume={}", native_volume);
+    R_SUCCEED();
+}
+
+Result IAudioController::NotifyAudioOutputTargetForPlayReport(u32 target) {
+    LOG_WARNING(Audio, "(STUBBED) called, target={}", target);
+    R_SUCCEED();
+}
+
+Result IAudioController::NotifyAudioOutputChannelCountForPlayReport(u32 channel_count) {
+    LOG_WARNING(Audio, "(STUBBED) called, channel_count={}", channel_count);
+    R_SUCCEED();
+}
+
+Result IAudioController::NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport() {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    R_SUCCEED();
+}
+
+Result IAudioController::BindAudioVolumeUpdateEventForPlayReport(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::BindAudioOutputTargetUpdateEventForPlayReport(OutCopyHandle<Kernel::KReadableEvent> out_event) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_event = &notification_event->GetReadableEvent();
+    R_SUCCEED();
+}
+
+Result IAudioController::GetDefaultAudioOutputTargetForPlayReport(Out<u32> out_target) {
+    LOG_WARNING(Audio, "(STUBBED) called");
+    *out_target = 0;
+    R_SUCCEED();
+}
+
+Result IAudioController::SetAnalogInputBoostGainForPrototyping(f32 gain) {
+    LOG_WARNING(Audio, "(STUBBED) called, gain={}", gain);
     R_SUCCEED();
 }
 

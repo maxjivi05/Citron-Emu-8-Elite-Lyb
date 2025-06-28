cdcda1fa5b7f4737e4b40fa083de6ffbc6b54c88
Zephyron <zephyron@citron-emu.org>
2025-06-28 19:55:42 +1000

    feat(core): Update HOS and NintendoSDK version constants
    - Bumps HOS version from 20.1.1 to 20.1.5
    - Updates the corresponding `VERSION_HASH`, `DISPLAY_VERSION`, and
      `DISPLAY_TITLE` strings.
    
    Signed-off-by: Zephyron <zephyron@citron-emu.org>

--- src/core/hle/api_version.h
+++ src/core/hle/api_version.h
@@ -14,7 +14,7 @@ namespace HLE::ApiVersion {
 
 constexpr u8 HOS_VERSION_MAJOR = 20;
 constexpr u8 HOS_VERSION_MINOR = 1;
-constexpr u8 HOS_VERSION_MICRO = 1;
+constexpr u8 HOS_VERSION_MICRO = 5;
 
 // NintendoSDK version constants.
 
@@ -22,9 +22,9 @@ constexpr u8 SDK_REVISION_MAJOR = 1;
 constexpr u8 SDK_REVISION_MINOR = 0;
 
 constexpr char PLATFORM_STRING[] = "NX";
-constexpr char VERSION_HASH[] = "9ffad64d79dd150490201461bdf66c8db963f57d";
-constexpr char DISPLAY_VERSION[] = "20.1.1";
-constexpr char DISPLAY_TITLE[] = "NintendoSDK Firmware for NX 20.1.1-1.0";
+constexpr char VERSION_HASH[] = "0605c36a7aa2535fb8989a0d133a0b96b0d97a12";
+constexpr char DISPLAY_VERSION[] = "20.1.5";
+constexpr char DISPLAY_TITLE[] = "NintendoSDK Firmware for NX 20.1.5-1.0";
 
 // Atmosphere version constants.
 

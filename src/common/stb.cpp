be70def4226fcd85d1635d88578fe8f48a12306e
collecting <collecting@citron-emu.org>
2025-09-23 05:01:27 +0000

    #define STBI_NO_SIMD

--- src/common/stb.cpp
+++ src/common/stb.cpp
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: Copyright 2023 yuzu Emulator Project
+// SPDX-FileCopyrightText: Copyright 2025 citron Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #define STBI_NO_SIMD
